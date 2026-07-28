7cb01e87685e69490898dbae1f7bae4200e7a448
rohitrawate <rohitrawate007@gmail.com>
2026-07-28 16:51:31 +0530

    feat: add common infrastructure and JPA auditing

---
+++ src/main/java/com/rohit/expensetracker/common/PageResponse.java
@@ -0,0 +1,20 @@
+package com.rohit.expensetracker.common;
+
+import java.util.List;
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class PageResponse<T> {
+
+    private List<T> content;
+
+    private int page;
+
+    private int size;
+
+    private long totalElements;
+
+    private int totalPages;
+}

8ab58bdb015dbde03c76c884575ff229ab613787
rohitrawate <rohitrawate007@gmail.com>
2026-07-30 00:06:55 +0530

    feat: created and added fields to entities

--- src/main/java/com/rohit/expensetracker/entity/User.java
+++ src/main/java/com/rohit/expensetracker/entity/User.java
@@ -1,10 +1,57 @@
 package com.rohit.expensetracker.entity;
 
 import com.rohit.expensetracker.common.BaseEntity;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
+import lombok.*;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder(toBuilder = true)
 @Entity
 @Table(name = "users")
 public class User extends BaseEntity {
+
+    @Column(name = "first_name", nullable = false, length = 100)
+    private String firstName;
+
+    @Column(name = "last_name", nullable = false, length = 100)
+    private String lastName;
+
+    @Column(name = "email", nullable = false, unique = true, length = 255)
+    private String email;
+
+    @Column(name = "password", nullable = false, length = 255)
+    private String password;
+
+    @Column(name = "enabled", nullable = false)
+    private boolean enabled = true;
+
+    @Column(name = "account_non_locked", nullable = false)
+    private boolean accountNonLocked = true;
+
+    @ManyToMany(fetch = FetchType.LAZY)
+    @JoinTable(
+            name = "user_roles",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id")
+    )
+    @Builder.Default
+    private Set<Role> roles = new HashSet<>();
+
+//    One user can have multiple refresh tokens (e.g., logged in from multiple devices)
+    @OneToMany(
+            mappedBy = "user",
+            cascade = CascadeType.ALL,
+            orphanRemoval = true    // If a refresh token is removed from the refreshTokens collection,
+                                    // Hibernate automatically deletes the corresponding row from the database.
+    )
+    @Builder.Default
+    private List<RefreshToken> refreshTokens = new ArrayList<>();
 }

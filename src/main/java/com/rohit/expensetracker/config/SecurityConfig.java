ce6ecefedf64b210fdd5ff70eba4a88bb7432ebf
rohitrawate <rohitrawate007@gmail.com>
2026-07-31 15:10:42 +0530

    feat(auth): configure Spring Security filter chain

---
+++ src/main/java/com/rohit/expensetracker/config/SecurityConfig.java
@@ -0,0 +1,28 @@
+package com.rohit.expensetracker.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+            http
+                .authorizeHttpRequests(auth ->
+                    auth.requestMatchers(
+                            "/api/v1/auth/**",
+                            "/actuator/health"
+                    ).permitAll()
+                     .anyRequest()
+                     .authenticated()
+            )
+             .httpBasic(Customizer.withDefaults());
+
+        return http.build();
+    }
+}

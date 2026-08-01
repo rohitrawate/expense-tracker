62013c6ae10aad55e944a44b15ecac0d44a784c2
rohitrawate <rohitrawate007@gmail.com>
2026-08-01 15:28:38 +0530

    feat(auth): implement HTTP Basic authentication with in-memory user store

--- src/main/java/com/rohit/expensetracker/config/SecurityConfig.java
+++ src/main/java/com/rohit/expensetracker/config/SecurityConfig.java
@@ -1,9 +1,16 @@
 package com.rohit.expensetracker.config;
 
+import org.jspecify.annotations.Nullable;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -25,4 +32,22 @@ public class SecurityConfig {
 
         return http.build();
     }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    UserDetailsService userDetailsService() {
+
+        UserDetails user1 =
+                User.withUsername("rohit")
+                        .password( passwordEncoder().encode("password") )
+                        .roles("USER")
+                        .build();
+
+        return new InMemoryUserDetailsManager(user1);
+    }
+
 }

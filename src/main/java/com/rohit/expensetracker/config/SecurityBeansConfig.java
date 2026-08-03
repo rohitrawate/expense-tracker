00e67409d0a8dbda46319dcd86a623034c9480ba
rohitrawate <rohitrawate007@gmail.com>
2026-08-03 23:41:15 +0530

    feat(auth): configure DaoAuthenticationProvider with database authentication

--- src/main/java/com/rohit/expensetracker/config/SecurityBeansConfig.java
+++ src/main/java/com/rohit/expensetracker/config/SecurityBeansConfig.java
@@ -1,15 +1,32 @@
 package com.rohit.expensetracker.config;
 
+import com.rohit.expensetracker.security.CustomUserDetailsService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
+@RequiredArgsConstructor
 public class SecurityBeansConfig {
 
-//    @Bean
+    private final CustomUserDetailsService userDetailsService;
+
+    @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationProvider authenticationProvider() {
+
+        DaoAuthenticationProvider provider =
+                new DaoAuthenticationProvider(userDetailsService);
+
+        provider.setPasswordEncoder(passwordEncoder());
+        return provider;
     }
 }

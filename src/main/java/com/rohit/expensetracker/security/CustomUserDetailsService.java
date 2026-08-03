5bc66350d1446eaccd0f9b5cd3c84e3861ec77e0
rohitrawate <rohitrawate007@gmail.com>
2026-08-03 17:16:24 +0530

    feat(auth): implement CustomUserDetailsService

---
+++ src/main/java/com/rohit/expensetracker/security/CustomUserDetailsService.java
@@ -0,0 +1,25 @@
+package com.rohit.expensetracker.security;
+
+import com.rohit.expensetracker.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        return userRepository.findByEmail(email)
+                .orElseThrow(() ->
+                        new UsernameNotFoundException(
+                                "User not found with email: " + email));
+    }
+}

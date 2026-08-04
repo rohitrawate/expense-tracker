b494b99a91045414070e13707dd52ac3f4287619
rohitrawate <rohitrawate007@gmail.com>
2026-08-04 20:54:08 +0530

    feat(auth): configure AuthenticationManager bean

--- src/main/java/com/rohit/expensetracker/config/SecurityBeansConfig.java
+++ src/main/java/com/rohit/expensetracker/config/SecurityBeansConfig.java
@@ -4,8 +4,10 @@ import com.rohit.expensetracker.security.CustomUserDetailsService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
@@ -28,5 +30,13 @@ public class SecurityBeansConfig {
 
         provider.setPasswordEncoder(passwordEncoder());
         return provider;
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(
+            AuthenticationConfiguration configuration)
+            throws Exception {
+
+        return configuration.getAuthenticationManager();
     }
 }

2b4c4937bbb59358e0c3f5a870d039bad11ad935
rohitrawate <rohitrawate007@gmail.com>
2026-08-09 18:53:12 +0530

    feat(auth): implement global exception handling for registration

--- src/main/java/com/rohit/expensetracker/config/SecurityConfig.java
+++ src/main/java/com/rohit/expensetracker/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -13,6 +14,7 @@ public class SecurityConfig {
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
             http
+                .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth ->
                     auth.requestMatchers(
                             "/api/v1/auth/**",

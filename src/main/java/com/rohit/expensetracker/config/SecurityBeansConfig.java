beedb02802af1d27dfecbf10929ae330cbe050e8
rohitrawate <rohitrawate007@gmail.com>
2026-08-15 00:15:35 +0530

    feat(login): configure authentication manager

--- src/main/java/com/rohit/expensetracker/config/SecurityBeansConfig.java
+++ src/main/java/com/rohit/expensetracker/config/SecurityBeansConfig.java
@@ -1,21 +1,17 @@
 package com.rohit.expensetracker.config;
 
-import com.rohit.expensetracker.security.CustomUserDetailsService;
-import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
-@RequiredArgsConstructor
 public class SecurityBeansConfig {
-
-    private final CustomUserDetailsService userDetailsService;
 
     @Bean
     public PasswordEncoder passwordEncoder() {
@@ -23,12 +19,15 @@ public class SecurityBeansConfig {
     }
 
     @Bean
-    public AuthenticationProvider authenticationProvider() {
+    public AuthenticationProvider authenticationProvider(
+            UserDetailsService userDetailsService,
+            PasswordEncoder passwordEncoder) {
 
         DaoAuthenticationProvider provider =
                 new DaoAuthenticationProvider(userDetailsService);
 
-        provider.setPasswordEncoder(passwordEncoder());
+        provider.setPasswordEncoder(passwordEncoder);
+
         return provider;
     }
 

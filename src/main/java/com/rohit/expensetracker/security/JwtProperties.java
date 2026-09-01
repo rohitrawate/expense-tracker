fce59281185c05f26337dcf2f65624d3c820592e
rohitrawate <rohitrawate007@gmail.com>
2026-09-01 12:51:17 +0530

    feat(jwt): issue JWT access token

--- src/main/java/com/rohit/expensetracker/security/JwtProperties.java
+++ src/main/java/com/rohit/expensetracker/security/JwtProperties.java
@@ -2,7 +2,7 @@ package com.rohit.expensetracker.security;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
-@ConfigurationProperties(prefix = "application.security.jwt")
+@ConfigurationProperties(prefix = "spring.security.jwt")
 public record JwtProperties(
       String secret,
       long expiration

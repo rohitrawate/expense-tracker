6e4c769295bb6eb46697b431578262af9ada59a2
rohitrawate <rohitrawate007@gmail.com>
2026-09-15 17:21:34 +0530

    feat(jwt): configure stateless JWT security

--- src/main/java/com/rohit/expensetracker/config/SecurityConfig.java
+++ src/main/java/com/rohit/expensetracker/config/SecurityConfig.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
@@ -21,19 +22,26 @@ public class SecurityConfig {
 
             http
                 .csrf(AbstractHttpConfigurer::disable)
+
+                .sessionManagement( session ->
+                        session.sessionCreationPolicy(
+                                SessionCreationPolicy.STATELESS
+                        ))
+
                 .authorizeHttpRequests(auth ->
-                    auth.requestMatchers(
-                            "/api/v1/auth/**",
-                            "/actuator/health"
+                    auth
+                        .requestMatchers(
+                        "/api/v1/auth/**",
+                        "/actuator/health"
                     ).permitAll()
                      .anyRequest()
                      .authenticated()
             )
-             .httpBasic(Customizer.withDefaults())
-                    .addFilterBefore(
-                            jwtAuthenticationFilter,
-                            UsernamePasswordAuthenticationFilter.class
-                    );
+//             .httpBasic(Customizer.withDefaults())
+                .addFilterBefore(
+                    jwtAuthenticationFilter,
+                    UsernamePasswordAuthenticationFilter.class
+                );
 
         return http.build();
     }
